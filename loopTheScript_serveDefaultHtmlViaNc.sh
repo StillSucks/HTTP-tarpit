dbb34d2e14db26190d7582934747370d5037c814
still.sucks <still.sucks@protonmail.com>
2023-03-24 21:19:17 +0000

    implemented parallel shadap processes to nginx and loop.sh

--- loopTheScript_serveDefaultHtmlViaNc.sh
+++ loopTheScript_serveDefaultHtmlViaNc.sh
@@ -2,12 +2,31 @@
 
 ## Customizeable defaults
 delay=60
+# The variable $usedPorts may be overwritten by the contents of $nginxFile
+usedPorts="8080 8081 8082"
+nginxFile="./nginx-defaultsite"
+# nginxFile="/etc/nginx/sites-enabled/default"
 
 ## Dependant variables
 # Subordinate script, that manages netcat, pv (slow data streaming), and the HTTP-response headers
 secondaryScript="./serveDefaultHtmlViaNc.sh"
 
-# create a self-looping worker
-while /bin/true ; do
-    timeout "$delay" bash "$secondaryScript" "8080" "$((delay - 2))"
-done &
+# Nginx / Reverse-Proxy dependant variable
+# If nginx config file exists/is readable:
+#    first grep:  parse the whole 'upstream *name* {*list-of-servers*}' block
+#    second grep: parse the port numbers (individual lines)
+[ -r "$nginxFile" ] && \
+    usedPorts=$(grep -Po -z "upstream [\w\s\-]+\{[^\}]*\}" "$nginxFile" \
+        | grep -Po -a "(?<=server localhost:)\d+" \
+        | tr '\n' ' ')
+
+
+# create a self-looping worker for each port
+for port in $usedPorts ; do
+    while /bin/true ; do
+        timeout "$delay" bash "$secondaryScript" "$port" "$((delay - 2))"
+    done &
+
+    # create an offset to prohibit simultaneous timeouts
+    sleep 1
+done
